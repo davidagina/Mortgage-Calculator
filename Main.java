29147ccbfe0f277e1de11e776c09878a046fa769
David Agina <davidagina64@gmail.com>
2022-03-11 14:21:53 +0100

    Mortgage calculator with validation

--- Main.java
+++ Main.java
@@ -10,18 +10,45 @@ public class Main {
         final byte MONTHS_IN_A_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while (true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000) {
+                break;
+            }
+            else
+                System.out.println("Enter a value between 1000 and 1_000_000");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        Double annualInterest = scanner.nextDouble();
-        Double monthlyInterest = annualInterest / PERCENT / MONTHS_IN_A_YEAR;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_A_YEAR;
+                break;
+            }
+            else
+                System.out.println("Enter a value between 1 and 30");
+        }
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_A_YEAR;
+        while (true) {
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_A_YEAR;
+                break;
+            }
+            else
+                System.out.println("Enter a value between 1 to 30");
+        }
+
 
         double mortgage = principal * (monthlyInterest * Math.pow((1 + monthlyInterest), numberOfPayments))
                 / ((Math.pow((1 + monthlyInterest), numberOfPayments)) - 1);
